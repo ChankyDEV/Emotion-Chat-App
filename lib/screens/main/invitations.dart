2b385508a66acba4bfa4fd5e16dfe88f603be97c
Kamil_Koczan <kamil.koczan@naviparking.com>
2021-09-25 19:27:58 +0200

    Changed invitations screen ui

--- lib/screens/main/invitations.dart
+++ lib/screens/main/invitations.dart
@@ -44,19 +44,51 @@ class Invitations extends StatelessWidget {
     int index,
     List<Inviter> inviters,
   ) {
+    final inviter = inviters[index];
     return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
       height: 80,
       child: Row(
         children: [
-          Text(
-            inviters[index].sender.name.value,
-            style: titleStyle,
+          inviter.sender.hasOwnImage
+              ? Container(
+                  height: 70.0,
+                  width: 60.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(inviter.sender.profileImage.url),
+                      fit: BoxFit.fill,
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                )
+              : Container(
+                  width: 60,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+          const SizedBox(
+            width: 15,
           ),
-          const SizedBox(width: 30,),
-          Text(
-            inviters[index].invitation.createdAt.toLocal().toString(),
-            style: bodyStyle,
-          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                inviter.sender.name.value,
+                style: titleStyle.copyWith(fontSize: 17.0),
+              ),
+              const SizedBox(height: 5,),
+              Text(
+                inviter.sender.emailAddress.value,
+                style: bodyStyle.copyWith(fontSize: 12.0, color: cWhite.withOpacity(0.5)),
+              ),
+            ],
+          )
+
         ],
       ),
     );
