1776f2742d6f9e81c6af957256c2a6f6baf1bac4
Kamil_Koczan <kamil.koczan@naviparking.com>
2021-10-04 18:52:27 +0200

    Implemented simple active chat screen

--- lib/screens/chat/active_chat.dart
+++ lib/screens/chat/active_chat.dart
@@ -1,6 +1,7 @@
 import 'package:emotion_chat/constants/data.dart';
 import 'package:emotion_chat/screens/core/consts/colors.dart';
 import 'package:emotion_chat/screens/core/consts/styles.dart';
+import 'package:emotion_chat/screens/core/widgets/my_text_field.dart';
 import 'package:flutter/material.dart';
 
 class ActiveChat extends StatelessWidget {
@@ -17,13 +18,146 @@ class ActiveChat extends StatelessWidget {
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: cDarkGrey,
+        title: _buildTitle(),
       ),
-      body: Center(
-        child: Text(
-          userToChatWith.name.value,
-          style: titleStyle,
+      body: Column(
+        children: [
+          Expanded(
+            flex: 7,
+            child: _buildMessages(),
+          ),
+          _buildTextInput(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Row(
+      children: [
+        Container(
+          height: 35.0,
+          width: 35.0,
+          decoration: BoxDecoration(
+            image: userToChatWith.hasOwnImage
+                ? DecorationImage(
+                    image: NetworkImage(userToChatWith.profileImage.url),
+                    fit: BoxFit.fill,
+                  )
+                : null,
+            shape: BoxShape.circle,
+          ),
         ),
+        const SizedBox(
+          width: 10,
+        ),
+        Text(
+          userToChatWith.name.value,
+          style: bodyStyle.copyWith(fontSize: 12.0),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMessages() {
+    final messages = <Message>[
+      Message(
+        content: Content(value: 'Yes, im here!'),
+        dateTime: DateTime.now().subtract(const Duration(seconds: 1)),
+        senderID: '12345',
+        isRead: false,
       ),
+      Message(
+        content: Content(value: 'Are you there?'),
+        dateTime: DateTime.now().subtract(const Duration(seconds: 5)),
+        senderID: userToChatWith.uid,
+        isRead: false,
+      ),
+      Message(
+        content: Content(value: 'Hello'),
+        dateTime: DateTime.now().subtract(const Duration(seconds: 10)),
+        senderID: userToChatWith.uid,
+        isRead: false,
+      ),
+      Message(
+        content: Content(value: 'What u want?'),
+        dateTime: DateTime.now(),
+        senderID: '12345',
+        isRead: false,
+      ),
+    ];
+
+    messages.sort(
+      (a, b) => a.dateTime.compareTo(b.dateTime),
+    );
+    final reversedMessages = messages.reversed.toList();
+    return ListView.builder(
+      reverse: true,
+      itemCount: reversedMessages.length,
+      itemBuilder: (context, index) {
+        return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5),
+          alignment: reversedMessages[index].senderID != userToChatWith.uid
+              ? Alignment.centerRight
+              : Alignment.centerLeft,
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              reversedMessages[index].content.value,
+              style: bodyStyle.copyWith(
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: cLightGrey,
+              borderRadius: BorderRadius.circular(4.0),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildTextInput() {
+    return Row(
+      children: [
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          flex: 10,
+          child: Container(
+            height: 80,
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: MyTextField(
+              hint: 'Aa...',
+              inputType: InputType.emailAddress,
+              controller: TextEditingController(),
+              suffixIcon: Icons.insert_emoticon_outlined,
+              action: () {},
+              isTextVisible: true,
+              textInputAction: TextInputAction.done,
+              onSubmitted: (value) {},
+              validate: false,
+              useOutlineInputBorder: true,
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Center(
+            child: IconButton(
+              onPressed: () {
+                //TODO: SEND MESSAGE
+              },
+              icon: Icon(
+                Icons.send,
+                color: cWhite,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
